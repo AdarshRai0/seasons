b874afab6fc363474a4cc987d9232ae216fbb6d8
AdarshRai0 <91651054+AdarshRai0@users.noreply.github.com>
2022-04-04 17:12:41 +0530

    changed the div element

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import ReactDom from 'react-dom';
-class App extends React.Component{
-  render(){
-    
-      window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-      );
-    return <div>Latitude: </div>
+import ReactDom from "react-dom";
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude of user: </div>;
   }
 }
-ReactDom.render(<App />, document.querySelector('#root'))
-
+ReactDom.render(<App />, document.querySelector("#root"));
