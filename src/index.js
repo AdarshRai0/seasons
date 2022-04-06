35a2e492d29c497b69bdb2700549498ae2bb5d7d
AdarshRai0 <91651054+AdarshRai0@users.noreply.github.com>
2022-04-06 13:35:09 +0530

    Avoiding conditionals in render

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ class App extends React.Component {
       (err) => this.setState({ errorMessage: err.message })
     );
   }
-  render() {
+
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error : {this.state.errorMessage}</div>;
     }
@@ -20,6 +21,9 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 ReactDom.render(<App />, document.querySelector("#root"));
